3fb495d49a537043018fe07cc45fb7e5b9010e58
Will Auld <will.auld@gmail.com>
2021-02-14 20:16:14 -0800

    Finishing add of the filter radio button function

--- wga-collect-email-admin.php
+++ wga-collect-email-admin.php
@@ -38,14 +38,21 @@ function get_email_counts($records) {
 
 function wga_management() {
     // Manage menu
+    $filterrecords = "all";
+
     if(!current_user_can('manage_options')) {
 	    die('Access Denied');
     }
-    echo '<h2> WGA Collect Email List Manage Page </h2>';
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	    if (!empty($_POST["filterrecords"])) {
+            $filterrecords = sanitize_text_field($_POST["filterrecords"]);
+        }
+    }
 
     $list = get_email_list(); 
     $count_array = get_email_counts($list);
    
+    echo '<h2> WGA Collect Email List Manage Page </h2>';
     echo '<style>';
     echo 'div.ex3 {';
     //echo '  background-color: lightblue;';
@@ -77,19 +84,23 @@ function wga_management() {
 
 	echo '<div class="container">';
 	echo '  <h2>Display records:</h2>';
-	echo '  <form action="'.site_url(). '/' . esc_url( $_SERVER['REQUEST_URI'] ) . ' method="post">'.PHP_EOL;
+	echo '  <form action="" method="post">'.PHP_EOL;
 	//echo '  <form>';
 	echo '    <label for="all" class="radio-inline">';
-	echo '      <input id="all" type="radio" name="filterrecords" value="all" onChange="this.form.submit();" checked>All';
+    $t1 = ($filterrecords == "all") ? "checked" : "";
+	echo '      <input id="all" type="radio" name="filterrecords" value="all" onChange="this.form.submit();" '. $t1 .' >All';
 	echo '    </label>';
 	echo '    <label for="active" class="radio-inline">';
-	echo '      <input id="active" type="radio" name="filterrecords" value="active" onChange="this.form.submit();">Active';
+    $t1 = ($filterrecords == "active") ? "checked" : "";
+	echo '      <input id="active" type="radio" name="filterrecords" value="active" onChange="this.form.submit();"'.$t1.' >Active';
 	echo '    </label>';
 	echo '    <label for="unverified" class="radio-inline">';
-	echo '      <input id="unverified" type="radio" name="filterrecords" value="unverified" onChange="this.form.submit();">Unverified';
+    $t1 = ($filterrecords == "unverified") ? "checked" : "";
+	echo '      <input id="unverified" type="radio" name="filterrecords" value="unverified" onChange="this.form.submit();" '.$t1.'>Unverified';
 	echo '    </label>';
 	echo '    <label for="unsubscribed" class="radio-inline">';
-	echo '      <input id="unsubscribed" type="radio" name="filterrecords" value="unsubscribed" onChange="this.form.submit();">Unsubscribed';
+    $t1 = ($filterrecords == "unsubscribed") ? "checked" : "";
+	echo '      <input id="unsubscribed" type="radio" name="filterrecords" value="unsubscribed" onChange="this.form.submit();" '.$t1.'>Unsubscribed';
 	echo '    </label>';
 	echo '  </form>';
 	echo '</div><br>';
@@ -111,21 +122,36 @@ function wga_management() {
     echo '  </tr>';
 
 	foreach ($list as $record) {
-        //echo sprintf('<p>%4d: %20s %20s </p>', $record->id, $record->first_name, $record->last_name); 
-        //echo sprintf('<p>%4d: %20s %20s %20s</p>', $record["id"], $record["first_name"], $record["last_name"], $record["email"]); 
-        echo '<tr>';
-        echo '  <td>'.$record["id"].'</td>';
-        echo '  <td>'.$record["first_name"].'</td>';
-        echo '  <td>'.$record["last_name"].'</td>';
-        echo '  <td>'.$record["email"].'</td>';
-        echo '  <td>'.$record["source"].'</td>';
-        echo '  <td>'.$record["unsubscribed"].'</td>';
-        echo '  <td>'.$record["created_at"].'</td>';
-        echo '  <td>'.$record["updated_at"].'</td>';
-        echo '  <td>'.$record["is_verified"].'</td>';
-        echo '  <td>'.$record["vhash"].'</td>';
-        echo '</tr>';
-        //echo '<hr>';
+        $dorecord = 0;
+        if ($filterrecords == "all") {
+            $dorecord = 1;
+        } elseif ($filterrecords == "active") {
+	        if (($record["is_verified"] == 1) && ($record["unsubscribed"] == 0)){
+                $dorecord = 1;
+            } 
+        } elseif ($filterrecords == "unverified") {
+	        if ($record["is_verified"] == 0) {
+                $dorecord = 1;
+            }
+        } elseif ($filterrecords == "unsubscribed") {
+	        if ($record["unsubscribed"] == 1) {
+                $dorecord = 1;
+            }
+        }
+        if ($dorecord == 1) {
+	        echo '<tr>';
+	        echo '  <td>'.$record["id"].'</td>';
+	        echo '  <td>'.$record["first_name"].'</td>';
+	        echo '  <td>'.$record["last_name"].'</td>';
+	        echo '  <td>'.$record["email"].'</td>';
+	        echo '  <td>'.$record["source"].'</td>';
+	        echo '  <td>'.$record["unsubscribed"].'</td>';
+	        echo '  <td>'.$record["created_at"].'</td>';
+	        echo '  <td>'.$record["updated_at"].'</td>';
+	        echo '  <td>'.$record["is_verified"].'</td>';
+	        echo '  <td>'.$record["vhash"].'</td>';
+	        echo '</tr>';
+        }
     }
     echo '</div>';
     echo '</table>';
