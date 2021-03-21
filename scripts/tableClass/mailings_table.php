5e009e7959f3043754dcd56fe35771f1e27b2e71
willauld <will.auld@gmail.com>
2021-03-21 15:02:21 -0700

    First round of conversion edits from email to mailings

--- scripts/tableClass/mailings_table.php
+++ scripts/tableClass/mailings_table.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-WGA_Message_list dirived from WP_List_Table Class
+WGA_mailings_list dirived from WP_List_Table Class
 Plugin URI: https://www.sitepoint.com/using-wp_list_table-to-create-wordpress-admin-tables/
 Description: Based on Demo on how WP_List_Table Class works
 Version: 1.0
@@ -12,83 +12,37 @@ if ( ! class_exists( 'WP_List_Table' ) ) {
 	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
 }
 
-class WGA_Email_List extends WP_List_Table {
+class WGA_Mailings_List extends WP_List_Table {
 
 	/** Class constructor */
 	public function __construct() {
 
 		parent::__construct( [
-			'singular' => __( 'Email', 'sp' ), //singular name of the listed records
-			'plural'   => __( 'Emails', 'sp' ), //plural name of the listed records
+			'singular' => __( 'Mailing', 'sp' ), //singular name of the listed records
+			'plural'   => __( 'Mailings', 'sp' ), //plural name of the listed records
 			'ajax'     => false, //does this table support ajax?
-			//'screen'   => 'Messages'
 		] );
 
 	}
     protected function get_views() { 
         $status_links = array(
             "All"       => __("<a href='#'>All</a>",'my-plugin-slug'),
-            "Active" => __("<a href='#'>Active</a>",'my-plugin-slug'),
-            "Unverified"   => __("<a href='#'>Unverified</a>",'my-plugin-slug'),
-            "Unsubscribed"   => __("<a href='#'>Unsubscribed</a>",'my-plugin-slug'),
-            "SPAM"   => __("<a href='#'>SPAM</a>",'my-plugin-slug'),
+            "Completed" => __("<a href='#'>Completed</a>",'my-plugin-slug'),
+            "ToBeDone"   => __("<a href='#'>To Be Done</a>",'my-plugin-slug'),
         );
         return $status_links;
     }
 
 	/**
-	 * Display email edit form  
+	 * Display mailings edit form  
 	 *
 	 * @param object $record db row
 	 *
 	 * @return mixed
 	 */
 	public static function display_record_edit_form($record){
-		echo '<style>';
-		//echo '.edit-form {';
-  		//echo '	background-color: #F6F7F8;';
-  		//echo '	border: 1px solid #D6D9DC;';
-  		//echo '	border-radius: 3px;';
-  		//echo '	width: 80%;';
-  		//echo '	padding: 50px;';
-  		//echo '	margin: 0 0 40px 0;';
-		//echo '}';
-		echo 'label {';
-    	echo 'display: inline-block;';
-    	echo 'width:100px;';
-    	echo 'text-align: right;';
-		echo '}';
-		echo '</style>';
-
-		echo '<div style="border: 2px solid #D6D9DC; background-color: #F6F7F8; border-radius: 6px; width: 60%; ipadding: 50px; margin: 0 0 40px 0; text-align: center; " >';
-        echo '<H1>Email Record id: '.$record->id.'</H1><br>';
-
-        echo '<form method="POST" class="edit-form">';
-
-        echo '<label for="fname">First name:</label>';
-        echo '<input type="text" id="fname" name="fname" value="'.$record->first_name.'"><br><br>';
-
-        echo '<label for="lname">Last name:</label>';
-        echo '<input type="text" id="lname" name="lname" value="'.$record->last_name.'"><br><br>';
-
-        echo '<label for="email">Email:</label>';
-        echo '<input type="email" id="email" name="email" value="'.$record->email.'"><br><br>';
-
-        echo '<label for="src">Source:</label>';
-        echo '<input type="text" id="src" name="source" value="'.$record->source.'"><br><br>';
-
-        echo '<label for="unsub">Unsubscribed:</label>';
-        echo '<input type="text" id="unsub" name="unsubscribed" value="'.$record->unsubscribed.'"><br><br>';
-
-        echo '<label for="isv">Is Verified?:</label>';
-        echo '<input type="text" id="isv" name="is_verified" value="'.$record->is_verified.'"><br><br>';
-
-        echo '<label for="is_s">Is SPAM?</label>';
-        echo '<input type="text" id="is_s" name="is_spam" value="'.$record->is_spam.'"><br><br>';
-
-	    submit_button( 'Save Modified Record' );
-        echo '</form>';
-		echo '</div>';
+        // Call new_mailing -- assume $record is an id???? 
+        wga_new_mailing($record);
     }
 
 	/**
@@ -99,11 +53,11 @@ class WGA_Email_List extends WP_List_Table {
 	 *
 	 * @return mixed
 	 */
-	public static function get_email_list( $per_page = 5, $page_number = 1 ) {
+	public static function get_mailings_list( $per_page = 5, $page_number = 1 ) {
 
 		global $wpdb;
 
-		$sql = "SELECT * FROM {$wpdb->prefix}wga_contact_list";
+		$sql = "SELECT * FROM {$wpdb->prefix}wga_mailings_list";
 
 		if ( ! empty( $_REQUEST['orderby'] ) ) {
 			$sql .= ' ORDER BY ' . esc_sql( $_REQUEST['orderby'] );
@@ -120,14 +74,14 @@ class WGA_Email_List extends WP_List_Table {
 	}
 
 	/**
-	 * get an email record.
+	 * get an mailings record.
 	 *
 	 * @param int $id customer ID
 	 */
-	public static function get_email_record( $id ) {
+	public static function get_mailings_record( $id ) {
 		global $wpdb;
 
-		$sql = "SELECT * FROM {$wpdb->prefix}wga_contact_list WHERE id = $id";
+		$sql = "SELECT * FROM {$wpdb->prefix}wga_mailings_list WHERE mailings_id = $id";
 		$result = $wpdb->get_row( $sql );
         return $result;
 	}
@@ -137,54 +91,24 @@ class WGA_Email_List extends WP_List_Table {
 	 *
 	 * @param int $id customer ID
 	 */
-	public static function delete_email_record( $id ) {
+	public static function delete_mailings_record( $id ) {
 		global $wpdb;
 
 		$wpdb->delete(
-			"{$wpdb->prefix}wga_contact_list",
-			[ 'id' => $id ],
+			"{$wpdb->prefix}wga_mailings_list",
+			[ 'mailings_id' => $id ],
 			[ '%d' ]
 		);
 	}
 
 	/**
-	 * Set SPAM bit in email record.
+	 * Edit update mailings record.
 	 *
-	 * @param int $id customer ID
+	 * @param int $id mailings ID
+     * 
+     * FIXME NEEDS TO BE CUSTOMIZED FOR MAILINGS
 	 */
-	public static function spam_email_record( $id ) {
-		global $wpdb;
-
-		$wpdb->update(
-			"{$wpdb->prefix}wga_contact_list",
-            [ 'is_spam' => 1 ],
-			[ 'id' => $id ],
-			[ '%d' ]
-		);
-	}
-
-	/**
-	 * Set unsubscribe bit in email record.
-	 *
-	 * @param int $id customer ID
-	 */
-	public static function unsubscribe_email_record( $id ) {
-		global $wpdb;
-
-		$wpdb->update(
-			"{$wpdb->prefix}wga_contact_list",
-            [ 'unsubscribed' => 1 ],
-			[ 'id' => $id ],
-			[ '%d' ]
-		);
-	}
-
-	/**
-	 * Edit update email record.
-	 *
-	 * @param int $id customer ID
-	 */
-	public function edit_update_email_record( $id, $fname, $lname, $email, $src, $unsub, $is_ver, $is_spam ) {
+	public function edit_update_mailings_record( $id, $fname, $lname, $email, $src, $unsub, $is_ver, $is_spam ) {
 		global $wpdb;
 		$wpdb->update(
 			"{$wpdb->prefix}wga_contact_list",
@@ -210,7 +134,7 @@ class WGA_Email_List extends WP_List_Table {
 	public static function record_count() {
 		global $wpdb;
 
-		$sql = "SELECT COUNT(*) FROM {$wpdb->prefix}wga_contact_list";
+		$sql = "SELECT COUNT(*) FROM {$wpdb->prefix}wga_mailings_list";
 
 		return $wpdb->get_var( $sql );
 	}
@@ -218,7 +142,7 @@ class WGA_Email_List extends WP_List_Table {
 
 	/** Text displayed when no data is available */
 	public function no_items() {
-		_e( 'No email records avaliable.', 'sp' );
+		_e( 'No mailings records avaliable.', 'sp' );
 	}
 
 
@@ -229,6 +153,8 @@ class WGA_Email_List extends WP_List_Table {
 	 * @param string $column_name
 	 *
 	 * @return mixed
+     * 
+     * FIXME NEEDS TO BE CUSTOMIZED FOR MAILINGS
 	 */
 	public function column_default( $item, $column_name ) {
 		switch ( $column_name ) {
@@ -283,18 +209,14 @@ class WGA_Email_List extends WP_List_Table {
 	 */
 	function column_id( $item ) {
 
-		$edit_nonce = wp_create_nonce( 'sp_edit_email_record' );
-		$delete_nonce = wp_create_nonce( 'sp_delete_email_record' );
-		$spam_nonce = wp_create_nonce( 'sp_SPAM_email_record' );
-		$unsub_nonce = wp_create_nonce( 'sp_unsubscribe_email_record' );
+		$edit_nonce = wp_create_nonce( 'sp_edit_mailings_record' );
+		$delete_nonce = wp_create_nonce( 'sp_delete_mailings_record' );
 
 		$title = '<strong>' . $item['id'] . '</strong>';
 
 		$actions = [
-            'edit' => sprintf( '<a href="?page=%s&action=%s&email_record=%s&_wpnonce=%s">Edit</a>', esc_attr( $_REQUEST['page'] ), 'edit', absint( $item['id'] ), $edit_nonce ),
-			'delete' => sprintf( '<a href="?page=%s&action=%s&email_record=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['id'] ), $delete_nonce ),
-			'SPAM' => sprintf( '<a href="?page=%s&action=%s&email_record=%s&_wpnonce=%s">SPAM</a>', esc_attr( $_REQUEST['page'] ), 'SPAM', absint( $item['id'] ), $spam_nonce ),
-			'Unsubscribe' => sprintf( '<a href="?page=%s&action=%s&email_record=%s&_wpnonce=%s">Unsubscribe</a>', esc_attr( $_REQUEST['page'] ), 'Unsubscribe', absint( $item['id'] ), $unsub_nonce ),
+            'edit' => sprintf( '<a href="?page=%s&action=%s&mailings_record=%s&_wpnonce=%s">Edit</a>', esc_attr( $_REQUEST['page'] ), 'edit', absint( $item['id'] ), $edit_nonce ),
+			'delete' => sprintf( '<a href="?page=%s&action=%s&mailings_record=%s&_wpnonce=%s">Delete</a>', esc_attr( $_REQUEST['page'] ), 'delete', absint( $item['id'] ), $delete_nonce ),
 		];
 
 		return $title . $this->row_actions( $actions );
@@ -305,21 +227,13 @@ class WGA_Email_List extends WP_List_Table {
 	 *  Associative array of columns
 	 *
 	 * @return array
+     * 
+     * FIXME NEEDS TO BE CUSTOMIZED FOR MAILINGS
 	 */
 	function get_columns() {
 		$columns = [
 			'cb'      => '<input type="checkbox" />',
 			'id'    => __( 'id', 'sp' ),
-			'First Name'    => __( 'First Name', 'sp' ),
-			'Last Name' => __( 'Last Name', 'sp' ),
-			'Email' => __( 'Email', 'sp' ),
-			'Source' => __( 'Source', 'sp' ),
-			'Unsubscribed' => __( 'Unsubscribed', 'sp' ),
-			'Is Verified?'    => __( 'Is Verified?', 'sp' ),
-			'Is SPAM?'    => __( 'Is SPAM?', 'sp' ),
-			'Created_at'    => __( 'Created_at', 'sp' ),
-			'Updated_at'    => __( 'Updated_at', 'sp' ),
-			'Hash'    => __( 'Hash', 'sp' ),
 		];
 
 		return $columns;
@@ -330,19 +244,15 @@ class WGA_Email_List extends WP_List_Table {
 	 * Columns to make sortable.
 	 *
 	 * @return array
+     * 
+     * FIXME NEEDS TO BE CUSTOMIZED FOR MAILINGS
 	 */
 	public function get_sortable_columns() {
 		$sortable_columns = array(
-			'id' => array( 'id', true ),
-			'First Name' => array( 'first_name', true ),
-			'Last Name' => array( 'last_name', true ),
-			'Email' => array( 'email', false ),
-			'Source' => array( 'source', false ),
-			'Unsubscribed' => array( 'unsubscribed', false ),
-			'Created_at' => array( 'created_at', true ),
-			'Updated_at' => array( 'updated_at', true ),
-			'Is Verified?' => array( 'is_verified', true ),
-			'Is SPAM?' => array( 'is_spam', true ),
+			'id' => array( 'mailings_id', true ),
+			'Created_at' => array( 'mailings_created_at', true ),
+			'Updated_at' => array( 'mailings_updated_at', true ),
+            // add start date
 		);
 
 		return $sortable_columns;
@@ -356,9 +266,6 @@ class WGA_Email_List extends WP_List_Table {
 	public function get_bulk_actions() {
 		$actions = [
 			'bulk-delete' => 'Delete',
-			'bulk-SPAM' => 'SPAM',
-			'bulk-unsubscribe' => 'Unsubscribe',
-			'bulk-resend-verify' => 'Resend Verify',
 		];
 
 		return $actions;
@@ -384,7 +291,7 @@ class WGA_Email_List extends WP_List_Table {
 			'per_page'    => $per_page //WE have to determine how many items to show on a page
 		] );
 
-		$this->items = self::get_email_list( $per_page, $current_page );
+		$this->items = self::get_mailings_list( $per_page, $current_page );
 	}
 
 	public function process_bulk_action() {
@@ -403,45 +310,25 @@ class WGA_Email_List extends WP_List_Table {
 		if ( 'edit' === $this->current_action() ) {
 			// In our file that handles the request, verify the nonce.
 			$nonce = esc_attr( $_REQUEST['_wpnonce'] );
-			if ( ! wp_verify_nonce( $nonce, 'sp_edit_email_record' ) ) {
+			if ( ! wp_verify_nonce( $nonce, 'sp_edit_mailings_record' ) ) {
 				die( 'Go get a life script kiddies' );
 			}
 			else {
-				$email_record_id = absint( $_GET['email_record'] ) ;
-				$record = self::get_email_record( $email_record_id );
-                echo 'id: '.$record->id;
-                self::display_record_edit_form($record);
+				$record_id = absint( $_GET['mailings_record'] ) ;
+				//$record = self::get_mailings_record( $record_id );
+                //echo 'id: '.$record->id;
+                self::display_record_edit_form($record_id);
 			}
             // SAVE operation is handled by on-page php code.
         }elseif ( 'delete' === $this->current_action() ) {
 			// In our file that handles the request, verify the nonce.
 			$nonce = esc_attr( $_REQUEST['_wpnonce'] );
-			if ( ! wp_verify_nonce( $nonce, 'sp_delete_email_record' ) ) {
+			if ( ! wp_verify_nonce( $nonce, 'sp_delete_mailings_record' ) ) {
 				die( 'Go get a life script kiddies' );
 			}
 			else {
-				$email_record_id = absint( $_GET['email_record'] ) ;
-				self::delete_email_record( $email_record_id );
-			}
-        }elseif ( 'SPAM' === $this->current_action() ) {
-			// In our file that handles the request, verify the nonce.
-			$nonce = esc_attr( $_REQUEST['_wpnonce'] );
-			if ( ! wp_verify_nonce( $nonce, 'sp_SPAM_email_record' ) ) {
-				die( 'Go get a life script kiddies' );
-			}
-			else {
-				$email_record_id = absint( $_GET['email_record'] ) ;
-				self::spam_email_record( $email_record_id );
-			}
-        }elseif ( 'Unsubscribe' === $this->current_action() ) {
-			// In our file that handles the request, verify the nonce.
-			$nonce = esc_attr( $_REQUEST['_wpnonce'] );
-			if ( ! wp_verify_nonce( $nonce, 'sp_unsubscribe_email_record' ) ) {
-				die( 'Go get a life script kiddies' );
-			}
-			else {
-				$email_record_id = absint( $_GET['email_record'] ) ;
-				self::unsubscribe_email_record( $email_record_id );
+				$record_id = absint( $_GET['mailings_record'] ) ;
+				self::delete_mailings_record( $record_id );
 			}
 		}
 
@@ -453,25 +340,7 @@ class WGA_Email_List extends WP_List_Table {
 
 			// loop over the array of record IDs and delete them
 			foreach ( $delete_ids as $id ) {
-				self::delete_email_record( $id );
-			}
-        } elseif ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-SPAM' )
-		     || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-SPAM' )
-		) {
-			$spam_ids = esc_sql( $_POST['bulk-ids'] );
-
-			// loop over the array of record IDs and set them as SPAM
-			foreach ( $spam_ids as $id ) {
-				self::spam_email_record( $id );
-			}
-        } elseif ( ( isset( $_POST['action'] ) && $_POST['action'] == 'bulk-unsubscribe' )
-		     || ( isset( $_POST['action2'] ) && $_POST['action2'] == 'bulk-unsubscribe' )
-		) {
-			$unsub_ids = esc_sql( $_POST['bulk-ids'] );
-
-			// loop over the array of record IDs and set them as SPAM
-			foreach ( $unsub_ids as $id ) {
-				self::unsubscribe_email_record( $id );
+				self::delete_mailings_record( $id );
 			}
 		}
 	}
@@ -481,13 +350,13 @@ class WGA_Email_List extends WP_List_Table {
 //
 // Instantiated by / near add_submenu_page('Manage') through get_instance()
 //
-class WGA_Manage_Email {
+class WGA_Manage_Mailings {
 
 	// class instance
 	static $instance;
 
 	// email WP_List_Table object
-	public $email_list_obj;
+	public $mailings_list_obj;
 
 	// class constructor
 	public function __construct() {
@@ -563,14 +432,14 @@ class WGA_Manage_Email {
 
 		$option = 'per_page';
 		$args   = [
-			'label'   => 'Email Records',
+			'label'   => 'Mailings Records',
 			'default' => 5,
 			'option'  => 'messages_per_page'
 		];
 
 		add_screen_option( $option, $args );
 
-		$this->email_list_obj = new WGA_Email_List();
+		$this->email_list_obj = new WGA_Mailings_List();
 	}
 
 
